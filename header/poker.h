ea98af0eb7a95e04c972f405ed95a34c4ac270c1
swpr0714 <swpr0714@gmail.com>
2024-05-18 19:16:16 +0800

    Move header file

--- header/poker.h
+++ header/poker.h
@@ -1,7 +1,12 @@
+#define cls system("cls")
+
 void swap(int *cardA, int *cardB);
 void quickSort(int* card, int left ,int right);
-int **shuffle(void);
+
 void printNum(int num);
 void printCard(int *card, int size);
 int int2str(int *card, char *buf);
 int str2int(int *card, char *buf);
+
+int **shuffle(void);
+int findOrder(int **card, SOCKET *sock);
