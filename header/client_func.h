4bfcf440dc8fdb5593ff91f0b22945e13caed8bc
swpr0714 <swpr0714@gmail.com>
2024-05-29 11:55:27 +0800

    CE bug fix

--- header/client_func.h
+++ header/client_func.h
@@ -7,11 +7,11 @@ int single(int *card, char *buffer);
 int pair(int *card, char *buffer);
 int checkStraight(int *card, int *num, int *prev);
 int straight(int *card, char *buffer);
-int checkFullHouse(int *card, int *num, int *prev)
+int checkFullHouse(int *card, int *num, int *prev);
 int fullHouse(int *card, char *buffer);
 int checkfourOfAKind(int *card, int *num, int *prev);
 int fourOfAKind(int *card, char *buffer);
 int checkFlush(int *card, int *num, int *prev);
 int flush(int *card, char *buffer);
-int gameOver(char *buffer)
+int gameOver(char *buffer);
 int clientShutdown();
