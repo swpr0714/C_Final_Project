d0a0c8996157c15ea686f9dcf2cab71c7e83703c
swpr0714 <swpr0714@gmail.com>
2024-06-05 14:38:06 +0800

    Modified header file

--- header/client_func.h
+++ header/client_func.h
@@ -1,4 +1,8 @@
 #pragma once
+typedef struct {
+    int rank;  // 數值3~2
+    int suit;  // 花色 梅花 方塊 紅心 黑桃
+} Card;
 int gameStart(char *buffer);
 int getCard(char *buffer, int *client_card);
 int recvCard(int *client_card, char *buffer);
@@ -7,6 +11,8 @@ int single(int *card, char *buffer);
 int pair(int *card, char *buffer);
 int checkStraight(int *card, int *num, int *prev);
 int straight(int *card, char *buffer);
+Card intToCard(int cardNum);
+void convertHand(int* hand, Card* cvrtHand, int handSize);
 int checkFullHouse(int *card, int *num, int *prev);
 int fullHouse(int *card, char *buffer);
 int checkfourOfAKind(int *card, int *num, int *prev);
